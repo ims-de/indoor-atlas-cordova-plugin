654889f6f4fa88c783c5e0d7ce09f6e7751bf336
Dominik Beerbohm <beerbohm@ims.de>
2023-02-10 17:52:01 +0100

    Don't use broken cordova bridge

--- src/android/src/main/java/com/ialocation/plugin/Utils.java
+++ src/android/src/main/java/com/ialocation/plugin/Utils.java
@@ -7,16 +7,57 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-import com.facebook.react.bridge.ReadableType;
-import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 
 import android.util.Log;
 
 class Utils
 {
+    public static JSONObject reactToJSON(ReadableMap readableMap) throws JSONException
+    {
+        JSONObject jsonObject = new JSONObject();
+        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+
+        while (iterator.hasNextKey()){
+            String key = iterator.nextKey();
+            ReadableType valueType = readableMap.getType(key);
+
+            switch (valueType){
+                case Null:
+                    jsonObject.put(key,JSONObject.NULL);
+                    break;
+
+                case Boolean:
+                    jsonObject.put(key, readableMap.getBoolean(key));
+                    break;
+
+                case Number:
+                    jsonObject.put(key, readableMap.getDouble(key));
+                    break;
+
+                case String:
+                    jsonObject.put(key, readableMap.getString(key));
+                    break;
+
+                case Map:
+                    jsonObject.put(key, reactToJSON(readableMap.getMap(key)));
+                    break;
+
+                case Array:
+                    jsonObject.put(key, reactToJSON(readableMap.getArray(key)));
+                    break;
+            }
+        }
+
+        return jsonObject;
+    }
+
     public static JSONArray reactToJSON(ReadableArray readableArray) throws JSONException
     {
         JSONArray jsonArray = new JSONArray();
